ba2a5497be9a967638166bc1ab5faea01ea30029
Carl Winkler <carl@carlossus.com>
2016-09-07 09:43:18 +0800

    Use aphrodite stylesheet for anchor element

--- front/react/components/anchor.tsx
+++ front/react/components/anchor.tsx
@@ -1,27 +1,25 @@
 import * as React from 'react';
 import * as DOM from 'react-dom';
+import {StyleSheet, css} from 'aphrodite';
 
+const Anchor = (props: { text: string, href: string }) => (
+    <a
+        className={css(styles.anchor, styles.hover)}
+        href={props.href}>
+        {props.text}
+    </a>
+)
 
-const Anchor = (props: { text: string, href: string }) => {
-
-    return (
-        <a
-            style={styles}
-            href={props.href}
-            onMouseEnter={enter} // hover('bold')
-            onMouseLeave={leave}>
-            {props.text}
-        </a>)
-}
-
-let styles: React.CSSProperties = {
-    color: 'white',
-    fontWeight: 'normal',
-    textDecoration: 'none'
-}
-type MouseEvent = React.MouseEvent<any> & { target: HTMLAnchorElement }
-
-const enter = (event: MouseEvent) => event.target.style.textDecoration = 'underline';
-const leave = (event: MouseEvent) => event.target.style.textDecoration = 'none'; 
+let styles = StyleSheet.create({
+    anchor: {
+        color: 'white',
+        textDecoration: 'none',                
+    },
+    hover: {
+        ':hover': {
+            textDecoration: 'underline'
+        }
+    }
+});
 
 export default Anchor;
