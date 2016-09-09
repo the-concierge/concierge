13c7312c7d2c8c6d8ad9591c72408e1ef440ec25
Carl Winkler <carl@carlossus.com>
2016-09-09 15:31:17 +0800

    Populate menu-data with data

--- front/components/shell/menu-data.tsx
+++ front/components/shell/menu-data.tsx
@@ -2,76 +2,90 @@ import * as React from 'react';
 
 const items: MenuItem[] = [
     {
-        name: 'Containers', href: '/containers',
+        text: 'Containers', href: '/containers',
         content: () => (<div>Containers...</div>),
         options: [
-            { name: 'Add Container', href: '/containers/new' },
-            { name: 'Refresh', href: '/containers' }
+            { text: 'Add Container', href: 'new' },
             // TODO: more...
         ]
     },
     {
-        name: 'Hosts', href: '/hosts',
+        text: 'Hosts', href: '/hosts',
         content: () => (<div>Hosts...</div>),
         options: [
-            { name: 'Add Host', href: '/hosts/new' },
+            { text: 'Add Host', href: 'new' },
             // TODO: more...
         ]
     },
     {
-        name: 'Images', href: '/images',
+        text: 'Images', href: '/images',
         content: () => (<div>Images...</div>),
         options: [
-            { name: 'Build Image', href: '/images/build' },
-            { name: 'Refresh', href: '/images' },
+            { text: 'Build Image', href: 'build' },
+            { text: 'Refresh', href: '/images' },
             // TODO: more...
         ]
     },
     {
-        name: 'Applications', href: '/applications',
+        text: 'Applications', href: '/applications',
         content: () => (<div>Applications...</div>),
         options: [
-            { name: 'Add Application', href: '/applications/new' },
-            { name: 'Refresh', href: '/applications' },
+            { text: 'Add Application', href: 'new' },
             // TODO: more...
         ]
     },
     {
-        name: 'Administration', href: '/admin',
+        text: 'Administration', href: '/admin',
         content: () => (<div>Administration...</div>),
         options: [
-            { name: 'Enable Editing', href: '/applications/new' },
-            { name: 'Refresh', href: '/applications' },
+            { text: 'Enable Editing', href: 'new' },
             // TODO: more...
         ]
     },
     {
-        name: 'Archive', href: '/archive',
+        text: 'Archive', href: '/archive',
         content: () => (<div>Archive...</div>),
         options: [
+
         ]
     },
-]
+];
 
+const fallback: MenuItem = {
+    text: 'Dashboard', href: '/',
+    content: () => <div>...</div>,
+    options: [
+        { text: 'Performance', href: 'performance' },
+        { text: 'Logs', href: 'logs' },
+    ]
+}
+
+
+
+export function getContent(category: string) {
+    return getItem(category).content;
+}
+
+export function getOptions(category: string) {
+    return getItem(category).options;
+}
+
+export function getItem(category: string): MenuItem {
+    return items.find(item => item.href.indexOf(category) >= 0) || fallback;
+}
+
+export function getTopLevel() {
+    return items.map(({text, href}) => ({ text, href }));
+}
 
 export default items;
 
 export interface MenuItem {
-    name: string;
+    text: string;
     href: string;
     content: () => JSX.Element;
     options: Array<{
-        name: string;
+        text: string;
         href: string;
     }>
 }
-
-// Legacy...
-const navItems = [
-    ['Hosts', '/hosts'],
-    ['Containers', '/containers'],
-    ['Images', '/react/images'],
-    ['Applications', '/applications'],
-    ['Administration', '/admin'],
-    ['Archive', '/archive']
-]
