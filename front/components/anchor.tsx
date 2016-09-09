7f6117726c45b7c965b8492d61a658a0224f5f34
Carl Winkler <carl@carlossus.com>
2016-09-09 14:50:19 +0800

    Allow children in Anchor

--- front/components/anchor.tsx
+++ front/components/anchor.tsx
@@ -1,19 +1,22 @@
 import * as React from 'react';
-import {Link} from 'react-router';
-import {StyleSheet, css} from 'aphrodite';
+import { Link } from 'react-router';
+import { StyleSheet, css } from 'aphrodite';
 
-export default ({text, href}: { text: string, href: string }) => (
-    <Link
-        className={css(styles.anchor, styles.hover)}
-        to={href}>
-        {text}
-    </Link>
-)
+type AnchorOpts = { text?: string, href: string, children?: any }
+export default ({text, href, children}: AnchorOpts) => {
+    return (
+        <Link
+            className={css(styles.anchor, styles.hover)}
+            to={href}>
+            {text || children}
+        </Link>
+    )
+}
 
 let styles = StyleSheet.create({
     anchor: {
         color: 'white',
-        textDecoration: 'none',                
+        textDecoration: 'none',
     },
     hover: {
         ':hover': {
