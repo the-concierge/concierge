d853c9542df1bcf49b3bdccb83b0339d7def7a90
Carl <carl@carlossus.com>
2016-09-15 21:08:32 +0800

    Add action types

---
+++ front/components/actions/types.ts
@@ -0,0 +1,31 @@
+enum ActionType {
+    AddContainer,
+    RemoveContainer,
+    AddHost,
+    RemoveHost,
+    // More...
+}
+
+export default ActionType;
+
+export interface Action<T> { kind: T }
+export type ActionTypes = AddContainer
+    | RemoveContainer
+    | AddHost
+    | RemoveHost
+    
+interface AddContainer extends Action<ActionType.AddContainer> {
+    container: Concierge.APIContainer;
+}
+
+interface RemoveContainer extends Action<ActionType.RemoveContainer> {
+    id: number;
+}
+
+interface AddHost extends Action<ActionType.AddHost> {
+    host: Concierge.APIHost;
+}
+
+interface RemoveHost extends Action<ActionType.RemoveHost> {
+    id: number;
+}
