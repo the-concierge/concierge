a5a7fb929bf9942ce29ce4c2b8443bda588f2dde
Carl <carl@carlossus.com>
2016-10-09 18:09:41 +0800

    Fix improper usage of paragraph tag in content

--- front/components/content/index.tsx
+++ front/components/content/index.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
  * TODO: Use Dashboard as default content
  */
 export default ({ children }: { children?: any }) => (
-    <p style={{ width: '100%', margin: 10 }}>
+    <div style={{ width: '100%', margin: 10 }}>
         {children || <div>...</div>}
-    </p>
+    </div>
 )
