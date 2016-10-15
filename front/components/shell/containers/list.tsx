103f282f578a30d2c69f7576afaef821f59c15f5
Carl <carl@carlossus.com>
2016-10-15 13:49:47 +0800

    Convert containers/hosts to table components

--- front/components/shell/containers/list.tsx
+++ front/components/shell/containers/list.tsx
@@ -1,13 +1,63 @@
 import * as React from 'react';
-import Single from './single';
+import Table from '../../table/index';
+import Head from '../../table/head';
+import Row from '../../table/row';
+import Anchor from '../../anchor';
+import { backgroundColor } from '../../common';
 
-// TODO: Simplify--remove duplicate code
 export default ({ containers }: AppState) => (
     <div>
-        {containers.map(container => (
-            <div key={container.id}>
-                <Single container={container} />
-            </div>
-        ))}
+        <Table>
+            <thead>
+                <Head headings={headings} rowStyles={headStyle} />
+            </thead>
+            <tbody>
+                {containers.map(toContainerRow)}
+            </tbody>
+        </Table>
     </div>
+);
+
+const toContainerRow = (container: Concierge.APIContainer) => (
+    <Row columns={toContainerColumns(container)} rowStyles={rowStyle} />
 )
+
+const toContainerColumns = (container: Concierge.APIContainer) => [
+    container.id,
+    container.label,
+    container.subdomain,
+    <Anchor href={`http://${container.host}:{container.port}`} styles={anchorStyle}>
+        {container.host}:{container.port}
+    </Anchor>,
+    container.applicationName,
+    container.dockerImage,
+    container.isProxying ? 'Yes' : 'No'
+]
+
+const headings = [
+    'Id',
+    'Label',
+    'Subdomain',
+    'Location',
+    'Application',
+    'Docker Image',
+    'Proxying'
+];
+
+const headStyle = {
+    tr: {
+        height: '45px'
+    }
+}
+
+const rowStyle = {
+    tr: {
+        height: '35px'
+    }
+}
+
+const anchorStyle = {
+    anchor: {
+        color: backgroundColor
+    }
+};
--- front/components/shell/hosts/list.tsx
+++ front/components/shell/hosts/list.tsx
@@ -1,13 +1,59 @@
 import * as React from 'react';
-import Single from './single';
+import Table from '../../table/index';
+import Head from '../../table/head';
+import Row from '../../table/row';
+import Anchor from '../../anchor';
+import { backgroundColor } from '../../common';
 
-// TODO: Simplify--remove duplicate code
 export default ({ hosts }: AppState) => (
     <div>
-        {hosts.map(host => (
-            <div key={host.id}>
-                <Single host={host} />
-            </div>
-        ))}
+        <Table>
+            <thead>
+                <Head headings={headings} rowStyles={headStyle} />
+            </thead>
+            <tbody>
+                {hosts.map(toHostRow)}
+            </tbody>
+        </Table>
     </div>
+);
+
+const toHostRow = (host: Concierge.APIHost) => (
+    <Row columns={toColumns(host)} rowStyles={rowStyle} />
 )
+
+const toColumns = (host: Concierge.APIHost) => [
+    host.id,
+    host.hostname,
+    host.dockerPort,
+    host.capacity,
+    host.containerCount || 0,
+    host.dockerVersion || 'Unknown'
+]
+
+const headings = [
+    'Id',
+    'Hostname',
+    'Docker Port',
+    'Capacity',
+    '# Containers',
+    'Docker Version'
+];
+
+const headStyle = {
+    tr: {
+        height: '45px'
+    }
+}
+
+const rowStyle = {
+    tr: {
+        height: '35px'
+    }
+}
+
+const anchorStyle = {
+    anchor: {
+        color: backgroundColor
+    }
+};
