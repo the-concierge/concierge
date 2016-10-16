9e24e84c0eb26d97c3fc897e9c5ad013754ba885
Carl <carl@carlossus.com>
2016-10-16 18:19:54 +0800

    Allow optional style params to Concierge block

--- front/components/shell/index.tsx
+++ front/components/shell/index.tsx
@@ -4,18 +4,21 @@ import Sidebar from '../sidebar/index';
 import Content from '../content/index';
 import * as menus from './navigation';
 import Body from './body';
+import mergeToClass from '../merge-style';
 
 type ConciergeParams = {
+    styles?: {};
     params: {
         category: string;
         item?: string;
     },
 }
-const Concierge = ({ params }: ConciergeParams) => (
-    <div style={styles}>
+
+const Concierge = ({ params, styles = {} }: ConciergeParams) => (
+    <div className={mergeToClass(conciergeStyles, styles)}>
         <Header items={menus.getTopLevel()} />
         <Body>
-            <Sidebar root={menus.getItem(params.category).href} options={menus.getOptions(params.category)} />
+            <Sidebar params={menus.getOptions(params.category)} />
             <Content>
                 {menus.getContent(params.category, params.item)}
             </Content>
@@ -25,7 +28,7 @@ const Concierge = ({ params }: ConciergeParams) => (
 
 export default Concierge;
 
-const styles: React.CSSProperties = {
+const conciergeStyles: React.CSSProperties = {
     fontFamily: 'Helvetica',
     display: 'flex',
     flexFlow: 'column',
