fe984c25c2ae2ea8d21e5fc65dd89ac93c6494b7
Carl <carl@carlossus.com>
2016-10-16 19:13:54 +0800

    Update entity if already exists in store

--- front/components/store/reducers/index.ts
+++ front/components/store/reducers/index.ts
@@ -7,27 +7,51 @@ export default function reduce(state: AppState = { containers: [], hosts: [] }, 
             return Object.assign(
                 {},
                 state,
-                { containers: [...state.containers, action.container] }
-            )
+                { containers: addEntity(state.containers, action.container) }
+            );
+
         case 'add-host':
             return Object.assign(
                 {},
                 state,
-                { hosts: [...state.hosts, action.host] }
-            )
+                { hosts: addEntity(state.hosts, action.host) }
+            );
+
         case 'remove-host':
             return Object.assign(
                 {},
                 state,
                 { hosts: state.hosts.filter(c => c.id !== action.id) }
-            )
+            );
+
         case 'remove-container':
             return Object.assign(
                 {},
                 state,
                 { containers: state.containers.filter(c => c.id !== action.id) }
-            )
+            );
+
         default:
             return state;
     }
+}
+
+type Id = { id?: string | number }
+
+/**
+ * Destructive
+ * Add or update
+ */
+function addEntity<T extends Id>(entities: Array<T>, entity: T) {
+    const existing = entities.find(c => c.id === entity.id);
+    if (existing) {
+        Object.assign(existing, entity);
+        return entities;
+    }
+    return entities.concat(entity);
+}
+
+function removeEntity<T extends Id>(state: AppState, entities: Array<T>, entityId: string | number) {
+    const filtered = entities.filter(entity => entity.id !== entityId);
+    return filtered;
 }
