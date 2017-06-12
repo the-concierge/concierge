0867fb6a46da3d6fda7c4e76d9ec9e57997f6017
Carl Winkler <carl@winkler.id.au>
2017-06-12 22:24:36 +0800

    Use new api in state manager - Add hosts call

--- front/components/state/index.ts
+++ front/components/state/index.ts
@@ -1,20 +1,33 @@
 import * as ko from 'knockout'
+import { ContainerInfo } from 'dockerode'
 
 class StateManager {
-  containers = ko.observableArray<Concierge.APIContainer>([])
+  containers = ko.observableArray<ContainerInfo>([])
   hosts = ko.observableArray<Concierge.APIHost>([])
   concierges = ko.observableArray<Concierge.Concierge>([])
   registries = ko.observableArray<Concierge.Registry>([])
 
   constructor() {
     this.getContainers()
+    this.getHosts()
   }
 
   getContainers = () =>
-    fetch('/api/container/running').then(res => res.json()).then(containers => {
-      this.containers.destroyAll()
-      this.containers.push(...containers)
-    })
+    fetch('/v2/hosts/containers')
+      .then(res => res.json())
+      .then(containers => {
+        this.containers.destroyAll()
+        this.containers.push(...containers)
+      })
+
+  getHosts = () => {
+    fetch('/v2/hosts')
+      .then(res => res.json())
+      .then(hosts => {
+        this.hosts.destroyAll()
+        this.hosts.push(...hosts)
+      })
+  }
 }
 
 const state = new StateManager()
