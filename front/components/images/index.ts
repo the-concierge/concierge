a66f60c2766d8784e0a150fc0894f1f1d808b49e
Carl Winkler <carl@winkler.id.au>
2017-07-07 15:53:59 +0800

    Implement dynamic image filtering by tag

--- front/components/images/index.ts
+++ front/components/images/index.ts
@@ -26,6 +26,16 @@ class Images {
   }
 
   images = state.images
+  imageFilter = ko.observable('')
+  filteredImages = ko.computed(() => {
+    const filter = this.imageFilter()
+    const images = this.images()
+    if (!filter) {
+      return images
+    }
+
+    return images.filter(image => image.name.indexOf(filter) > -1)
+  })
 
   toMb = (size: number) => `${common.round(size / 1024 / 1024, 2)}MB`
   toDate = (timestamp: number) => new Date(timestamp * 1000).toUTCString()
@@ -34,6 +44,24 @@ class Images {
   showModal = () => this.modalActive(true)
 
   refresh = () => state.getImages()
+
+  removeImage = async (image: Image) => {
+    const tag = image.name
+    const result = await fetch(`/api/images?tag=${tag}`, {
+      method: 'DELETE'
+    })
+
+    const msg = await result.json()
+
+    if (result.status < 400) {
+      state.toast.success(msg.message)
+      this.refresh()
+      return
+    }
+
+    state.toast.error(msg.message)
+    this.refresh()
+  }
 
   configureImage = async (image: Image) => {
     this.modalImage(image)
