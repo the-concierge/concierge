718cdbdcac3b49ff41e9375be30fd7cf00b307e4
Carl Winkler <carl@winkler.id.au>
2017-07-28 13:14:31 +0800

    Refactor container view models - Break out container inspect modal to separate component

--- front/components/containers/container-vm.ts
+++ front/components/containers/container-vm.ts
@@ -1,9 +1,11 @@
-import state, { ObservableContainer } from '../state'
 import * as ko from 'knockout'
 import * as fs from 'fs'
+import state from '../state'
+import inspect from './inspect'
 
 class Containers {
   containers = state.containers
+  inspectContainer = inspect.inspect
 
   hosts = ko.computed(() => {
     const hosts = state
@@ -17,6 +19,7 @@ class Containers {
   })
 
   selectedHost = ko.observable(this.hosts()[0])
+
   filteredContainers = ko.computed(() => {
     const host = this.selectedHost()
     const containers = this.containers()
@@ -26,89 +29,6 @@ class Containers {
 
     return containers.filter(container => container.host.id === host.id)
   })
-
-  modalActive = ko.observable(false)
-
-  currentContainer = ko.observable<string | undefined>()
-
-  defaultModalContainer: ObservableContainer = {
-    id: ko.observable('...'),
-    name: ko.observable('...'),
-    image: ko.observable('...'),
-    state: ko.observable('...'),
-    status: ko.observable('...'),
-    restart: ko.observable(''),
-    stats: {
-      mbIn: ko.observable('...'),
-      mbOut: ko.observable('...'),
-      cpu: ko.observable('...'),
-      memory: ko.observable('...')
-    },
-    ports: ko.observableArray([]),
-    host: {
-      capacity: 0,
-      dockerPort: 0,
-      hostname: '...',
-      id: 0,
-      vanityHostname: '...'
-    }
-  }
-
-  modalContainer = ko.computed((): ObservableContainer => {
-    const id = this.currentContainer()
-    if (!id) {
-      return this.defaultModalContainer
-    }
-
-    const container = this.containers().find(c => c.id().startsWith(id))
-    return container
-  })
-
-  showStartButton = ko.computed(() => this.modalContainer().state() === 'exited')
-  showStopButton = ko.computed(() => this.modalContainer().state() === 'running')
-  containerWaiting = ko.observable(false)
-  buttonLoading = ko.observable('')
-
-  /**
-   * TODO:
-   * - Add UI feedback for container control
-   * - Remove deleted containers somehow? If they don't appear in 'getContainers', just remove them?
-   */
-
-  loading = () => {
-    this.containerWaiting(true)
-  }
-
-  resetButtons = () => {
-    this.buttonLoading('')
-    this.containerWaiting(false)
-  }
-
-  modifyContainer = async (command: string, method: string = 'GET') => {
-    const container = this.modalContainer()
-    const route = `/api/containers/${container.id()}/${command}/${container.host.id}`
-    this.loading()
-    const res = await fetch(route, { method })
-    const result = await res.json()
-    if (res.status >= 400) {
-      state.toast.error(`Failed to start container: ${result.message}`)
-    }
-    this.resetButtons()
-    return result
-  }
-
-  stopContainer = async () => this.modifyContainer('stop')
-  startContainer = async () => this.modifyContainer('start')
-  removeContainer = async () => this.modifyContainer('host', 'DELETE')
-    .then(() => this.currentContainer(''))
-    .then(() => this.hideModal())
-
-  showModal = (container: ObservableContainer) => {
-    this.currentContainer(ko.unwrap(container).id())
-    this.modalActive(true)
-  }
-
-  hideModal = () => this.modalActive(false)
 }
 
 const viewModel = new Containers()
---
+++ front/components/containers/inspect/inspect-vm.ts
@@ -0,0 +1,95 @@
+import * as ko from 'knockout'
+import * as fs from 'fs'
+import state, { ObservableContainer } from '../../state'
+
+class Inspect {
+  containerId = ko.observable('')
+  modalActive = ko.observable(false)
+
+  defaultContainer: ObservableContainer = {
+    id: ko.observable('...'),
+    name: ko.observable('...'),
+    image: ko.observable('...'),
+    state: ko.observable('...'),
+    status: ko.observable('...'),
+    restart: ko.observable(''),
+    stats: {
+      mbIn: ko.observable('...'),
+      mbOut: ko.observable('...'),
+      cpu: ko.observable('...'),
+      memory: ko.observable('...')
+    },
+    ports: ko.observableArray([]),
+    host: {
+      capacity: 0,
+      dockerPort: 0,
+      hostname: '...',
+      id: 0,
+      vanityHostname: '...'
+    }
+  }
+
+  container = ko.computed(() => {
+    const id = this.containerId()
+    if (!id) {
+      return this.defaultContainer
+    }
+
+    const container = state
+      .containers()
+      .find(container => container.id() === id)
+
+    return container || this.defaultContainer
+  })
+
+  showStartButton = ko.computed(() => this.container().state() === 'exited')
+  showStopButton = ko.computed(() => this.container().state() === 'running')
+  containerWaiting = ko.observable(false)
+  buttonLoading = ko.observable('')
+
+  inspect = (container: ObservableContainer) => {
+    this.containerId(ko.unwrap(container.id))
+    this.modalActive(true)
+  }
+
+  hideModal = () => this.modalActive(false)
+
+  loading = () => {
+    this.containerWaiting(true)
+  }
+
+  resetButtons = () => {
+    this.buttonLoading('')
+    this.containerWaiting(false)
+  }
+
+  modifyContainer = async (command: string, method: string = 'GET') => {
+    const container = this.container()
+    const route = `/api/containers/${container.id()}/${command}/${container.host.id}`
+    this.loading()
+    const res = await fetch(route, { method })
+    const result = await res.json()
+    if (res.status >= 400) {
+      state.toast.error(`Failed to start container: ${result.message}`)
+    }
+    this.resetButtons()
+    return result
+  }
+
+  stopContainer = async () => this.modifyContainer('stop')
+  startContainer = async () => this.modifyContainer('start')
+  removeContainer = async () => this.modifyContainer('host', 'DELETE')
+    .then(() => this.containerId(''))
+    .then(() => this.hideModal())
+}
+
+const viewModel = new Inspect()
+
+ko.components.register('ko-inspect', {
+  template: fs.readFileSync(`${__dirname}/inspect.html`).toString(),
+  viewModel: {
+    createViewModel: () => viewModel
+  }
+})
+
+export default viewModel
