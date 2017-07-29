f00882f6811a6787cbe46192d4447529975b61b7
Carl Winkler <carl@carlossus.com>
2017-07-29 11:59:41 +0800

    Add windows socket support for hostnameless host

--- src/api/docker.ts
+++ src/api/docker.ts
@@ -1,10 +1,15 @@
 import * as DockerClient from 'dockerode'
+import * as process from 'process'
 
 export default function getDockerClient(host: Concierge.Host, timeout?: number) {
   // If no hostname is provided, try and use the local unix socket
   if (!host.hostname) {
+    const socketPath = process.platform === 'win32'
+      ? '//./pipe/docker_engine'
+      : '/var/run/docker.sock'
+
     const dockerClient = new DockerClient({
-      socketPath: '/var/run/docker.sock',
+      socketPath,
       timeout: timeout || 0
     })
     return dockerClient
