918eb341bb0b3cfbf68aace01ca642e22fb5580d
Carl Winkler <carl@carlossus.com>
2017-07-29 12:21:39 +0800

    Fix heartbeats internal type - Remove semicolons

--- typings/internal.d.ts
+++ typings/internal.d.ts
@@ -2,63 +2,63 @@
 /// <reference path="./analysis/analysis.d.ts" />
 
 interface Window {
-  containerPoller: any;
+  containerPoller: any
 }
 
 interface Logger {
-  info: (message: string) => void;
-  warn: (message: string) => void;
-  error: (message: string) => void;
-  debug: (message: string) => void;
+  info: (message: string) => void
+  warn: (message: string) => void
+  error: (message: string) => void
+  debug: (message: string) => void
 }
 
-declare const log: Logger;
+declare const log: Logger
 
 declare module "toastr" {
-  var toastr: Toastr;
-  export = toastr;
+  var toastr: Toastr
+  export = toastr
 }
 
 interface Toastr {
-  success(message: string): void;
-  info(message: string): void;
-  warning(message: string): void;
-  error(message: string): void;
-  options: any;
+  success(message: string): void
+  info(message: string): void
+  warning(message: string): void
+  error(message: string): void
+  options: any
 }
 
 declare module Concierge {
   interface Concierge {
-    id: number;
-    label: string;
-    hostname: string;
-    port: number;
+    id: number
+    label: string
+    hostname: string
+    port: number
   }
 
   interface HeartBeat {
-    containerId: number;
-    cpu: string;
-    memory: string;
-    responseTime: number;
-    timestamp: number;
+    hostId: string
+    containerId: number
+    cpu: string
+    memory: string
+    timestamp: number
   }
 
   interface Registry {
-    url: string;
-    getUntaggedImage: (application: Application) => string;
-    getTaggedImage: (application: Application, tag: string) => string;
+    url: string
+    getUntaggedImage: (application: Application) => string
+    getTaggedImage: (application: Application, tag: string) => string
   }
 
   interface Configuration {
-    name: string;
-    conciergePort: number;
-    proxyHostname?: string;
-    proxyIp?: string;
-    httpPort: number;
-    debug: number;
-    statsBinSize: number; // Samples per bin at 1Hert per container
-    statsRentionDays: number; // Truncate stats records older than N day(s)
-    dockerRegistry?: string;
+    name: string
+    conciergePort: number
+    proxyHostname?: string
+    proxyIp?: string
+    httpPort: number
+    debug: number
+    statsBinSize: number // Samples per bin at 1Hert per container
+    statsRentionDays: number // Truncate stats records older than N day(s)
+    dockerRegistry?: string
   }
 
   interface Application {
@@ -79,123 +79,123 @@ declare module Concierge {
   }
 
   interface NewContainer {
-    subdomain: string;
-    variant: string;
-    label: string;
-    host?: string;
-    volume?: Buffer;
-    dockerImage?: string;
-    applicationId?: number;
+    subdomain: string
+    variant: string
+    label: string
+    host?: string
+    volume?: Buffer
+    dockerImage?: string
+    applicationId?: number
 
     /** JSON representation of Array<string> */
-    variables: string;
+    variables: string
   }
 
   interface Host {
-    id?: number;
-    hostname: string;
+    id?: number
+    hostname: string
     vanityHostname: string
-    capacity: number;
-    dockerPort: number;
-    sshPort: number;
-    sshUsername: string;
-    // volumesPath: string;
-    privateKey?: string;
+    capacity: number
+    dockerPort: number
+    sshPort: number
+    sshUsername: string
+    // volumesPath: string
+    privateKey?: string
   }
 
   interface Entity {
-    type: string;
-    name: string;
+    type: string
+    name: string
   }
 
   interface Event extends Entity {
-    event: any;
+    event: any
   }
 
   interface Stats {
-    containerId: number;
-    cpu: Box;
-    memory: Box;
-    responseTime: number;
-    timestamp: number;
+    containerId: number
+    cpu: Box
+    memory: Box
+    responseTime: number
+    timestamp: number
   }
 
   interface Box {
-    mean: number;
-    mode: number[];
-    median: number;
+    mean: number
+    mode: number[]
+    median: number
     range: {
       minimum: number,
       maximum: number,
       difference: number
-    };
-    lowerQuartile: number;
-    upperQuartile: number;
+    }
+    lowerQuartile: number
+    upperQuartile: number
   }
 
   interface SourceControlApi {
-    getTags: (application: Application) => Promise<Array<string>>;
-    getRepository: (application: Application) => string;
-    privateBaseUrl: string;
-    publicBaseUrl: string;
+    getTags: (application: Application) => Promise<Array<string>>
+    getRepository: (application: Application) => string
+    privateBaseUrl: string
+    publicBaseUrl: string
   }
 
   interface Archive {
-    application: string;
-    filename: string;
-    subdomain: string;
-    timestamp: number;
-    variant: string;
-    date: string;
+    application: string
+    filename: string
+    subdomain: string
+    timestamp: number
+    variant: string
+    date: string
   }
 }
 
 declare module 'ip' {
-  const api: any;
-  export = api;
+  const api: any
+  export = api
 }
 
 declare module 'http-proxy' {
-  const api: any;
-  export = api;
+  const api: any
+  export = api
 }
 
 declare module 'ssh2' {
-  const api: any;
-  export = api;
+  const api: any
+  export = api
 }
 
 declare module 'tar-fs' {
   const api: {
-    pack(directory: string, options?: any): NodeJS.ReadableStream;
-    extract(directory: string, options?: any): NodeJS.WritableStream;
-  };
+    pack(directory: string, options?: any): NodeJS.ReadableStream
+    extract(directory: string, options?: any): NodeJS.WritableStream
+  }
 
-  export = api;
+  export = api
 }
 
 declare module 'inert' {
-  const api: any;
-  export = api;
+  const api: any
+  export = api
 }
 
 declare module 'rimraf' {
-  const api: any;
-  export = api;
+  const api: any
+  export = api
 }
 
 declare module 'compression' {
   namespace api { }
-  function api(): any;
-  export = api;
+  function api(): any
+  export = api
 }
 
 declare module 'body-parser' {
   namespace api {
     function json(): any
   }
-  function api(): any;
-  export = api;
+  function api(): any
+  export = api
 }
 
 interface ConciergeEvent<T> {
