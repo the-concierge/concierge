333990d1510f95b76f9659f13b524a31978523ef
Carl Winkler <carl@winkler.id.au>
2017-08-29 14:12:14 +0800

    Remove key from application DTO

--- src/api/applications/get.ts
+++ src/api/applications/get.ts
@@ -3,10 +3,16 @@ import * as get from './db'
 
 export const one: RequestHandler = async (req, res) => {
   const application = await get.one(req.params.id)
+  application.key = ''
   res.json(application)
 }
 
 export const all: RequestHandler = async (req, res) => {
   const applications = await get.all()
+
+  for (const app of applications) {
+    app.key = ''
+  }
+
   res.json(applications)
 }
