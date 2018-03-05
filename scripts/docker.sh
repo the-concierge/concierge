f5919260a8019752e789ed9645db17bd6152435b
Carl Winkler <carl@carlossus.com>
2018-03-05 15:17:19 +0800

    Remove redundant scripts

--- scripts/docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-
-sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list'
-
-sudo apt-get update
-
-sudo apt-get install docker-engine -y
--- scripts/node.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -qO- https://deb.nodesource.com/setup_6.x | sudo bash -
-sudo apt-get install nodejs -y
